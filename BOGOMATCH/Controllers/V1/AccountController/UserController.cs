e171892d454911606febed7c555368548b699305
Muhammad Ali <Muhammad Ali@MUHAMMAD-ALI>
2025-06-19 04:44:10 -0400

    Add project files.

---
+++ BOGOMATCH/Controllers/V1/AccountController/UserController.cs
@@ -0,0 +1,63 @@
+﻿using AngularAuthAPI.Models.Dto;
+using BOGOGMATCH_DOMAIN.MODELS.UserManagement;
+using BOGOMATCH_DOMAIN.INTERFACE;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserAuthService _authService;
+
+    public UserController(IUserAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("Authenticate")]
+    public async Task<IActionResult> Authenticate([FromBody] User userObj)
+    {
+        var result = await _authService.AuthenticateAsync(userObj);
+        return Ok(result);
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody] User userObj)
+    {
+        var result = await _authService.RegisterUserAsync(userObj);
+        return Ok(new { Message = result });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _authService.GetAllUsersAsync();
+        if (!users.Any())
+        {
+            return NotFound("No users found.");
+        }
+        return Ok(users);
+    }
+
+    [HttpPost("Refresh")]
+    public async Task<IActionResult> Refresh([FromBody] TokenApiDTO tokenDto)
+    {
+        var result = await _authService.RefreshTokenAsync(tokenDto);
+        return Ok(result);
+    }
+
+    [HttpPost("Send-Reset-Email/{email}")]
+    public async Task<IActionResult> SendResetEmail(string email)
+    {
+        var result = await _authService.SendResetEmailAsync(email);
+        return Ok(new { Message = result });
+    }
+
+    [HttpPost("Reset-Password")]
+    public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDTO dto)
+    {
+        var result = await _authService.ResetPasswordAsync(dto);
+        return Ok(new { Message = result });
+    }
+}
