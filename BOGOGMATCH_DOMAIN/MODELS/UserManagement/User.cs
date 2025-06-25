7814f92b348fb336794e4140c5196e6a4b232c65
Muhammad Ali <hmaliqureshi91@gmail.com>
2025-06-25 21:58:46 +0500

    minor model changes

--- BOGOGMATCH_DOMAIN/MODELS/UserManagement/User.cs
+++ BOGOGMATCH_DOMAIN/MODELS/UserManagement/User.cs
@@ -8,7 +8,8 @@ namespace BOGOGMATCH_DOMAIN.MODELS.UserManagement
     {
         [Key]
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string? Password { get; set; }
         public string? Role { get; set; }
